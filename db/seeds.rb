0238aea12136eff322b0ebe94cd031f9df343135
Vu Dinh Hao <vu.dinh.hao@framgia.com>
2016-07-12 08:48:33 +0700

    fix bug

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,39 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+5.times do |n|
+  Subject.create! name: "Ruby" + (n+1).to_s,
+    chatwork_room_id: "123#{n+1}",
+    number_of_question: 5,
+    duration: 5
+end
+
+5.times do |n|
+  Exam.create! score: 4,
+    status: 1,
+    time: 20,
+    user_id: 1,
+    subject_id: n + 1
+end
+
+10.times do |n|
+  Question.create! content: "Content" + (n+1).to_s,
+    question_type: 1,
+    state: 1,
+    active: true,
+    subject_id: n+1,
+    user_id: 1
+end
+
+5.times do |n|
+  Result.create! correct: true,
+    question_id: n+1,
+    exam_id: n+1
+end
+5.times do |n|
+  Option.create! content: "content_option" + (n+1).to_s,
+    correct: true,
+    question_id: n+1
+end
+
+10.times do |n|
+  Answer.create! content: "content answer" + (n+1).to_s
+end
+
